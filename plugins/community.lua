e3e2898e8d7e02ca2200e35405769a99f45ca51e
Azizul Rahman Azizan <Azizul.Rahman.Azizan@gmail.com>
2023-05-11 12:03:02 +0800

    Add basic community package

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
