c744234a8a87d064f8b251249b44c1c13c07df55
Azizul Rahman Azizan <Azizul.Rahman.Azizan@gmail.com>
2023-05-31 13:42:16 +0800

    update colorscheme project rename; adds typescript, java and go

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,9 +4,12 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
