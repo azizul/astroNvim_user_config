d40c73209b594ce270e1807def431a5be160f49b
Azizul Rahman Azizan <Azizul.Rahman.Azizan@gmail.com>
2023-06-03 10:35:15 +0800

    Add one dark and catppuccin colorscheme; use onedark_dark as default

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,8 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
